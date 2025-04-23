aa59422d79326a11b307eb84d7d84b119b9abd0c
Gelande-1234 <angegelande@gmail.com>
2025-04-23 11:14:01 +0200

    stylisation du formulaire, compilation de bootstrap

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
--- src/Form/TicketType.php
+++ src/Form/TicketType.php
@@ -6,21 +6,45 @@ use App\Entity\Ticket;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class TicketType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('status')
-            ->add('priotity')
-            ->add('createdAt', null, [
-                'widget' => 'single_text',
+            ->add('title', null, [
+                'label' => 'Titre',
+                'attr' => ['class' => 'form-control'],
             ])
-            ->add('Tickets')
-        ;
+            ->add('description', null, [
+                'label' => 'Description',
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('status', ChoiceType::class, [
+                'choices' => [
+                    'Ouvert' => 'open',
+                    'En cours' => 'in_progress',
+                    'Fermé' => 'closed',
+                ],
+                'label' => 'Statut',
+                'attr' => ['class' => 'form-select'],
+            ])
+            ->add('priority', ChoiceType::class, [
+                'choices' => [
+                    'Basse' => 'low',
+                    'Normale' => 'normal',
+                    'Haute' => 'high',
+                ],
+                'label' => 'Priorité',
+                'attr' => ['class' => 'form-select'],
+            ])
+            ->add('createdAt', DateTimeType::class, [
+                'widget' => 'single_text',
+                'label' => 'Date de création',
+                'attr' => ['class' => 'form-control'],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -30,3 +54,4 @@ class TicketType extends AbstractType
         ]);
     }
 }
+
